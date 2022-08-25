a872bc3cc5fb6c80e6be7bd4ecbe96aae4fc142b
wilur <wilsonurbano2011@hotmail.com>
2022-08-25 18:20:11 -0500

    Creacion de la entidad Empresa

---
+++ src/main/java/com/FisiOnLine/main/models/Empresa.java
@@ -0,0 +1,55 @@
+package com.FisiOnLine.main.models;
+
+public class Empresa {
+    //Atributos
+    private String nombreEmpresa;
+    private String direccionEmpresa;
+    private String telefonoEmpresa;
+    private String nitEmpresa;
+    //Constructor con parametros
+
+    public Empresa(String nombreEmpresa, String direccionEmpresa, String telefonoEmpresa, String nitEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+        this.direccionEmpresa = direccionEmpresa;
+        this.telefonoEmpresa = telefonoEmpresa;
+        this.nitEmpresa = nitEmpresa;
+    }
+    //Creamos un contructor sin parametros
+
+    public Empresa() {
+    }
+
+    //Getters and Setters
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getDireccionEmpresa() {
+        return direccionEmpresa;
+    }
+
+    public void setDireccionEmpresa(String direccionEmpresa) {
+        this.direccionEmpresa = direccionEmpresa;
+    }
+
+    public String getTelefonoEmpresa() {
+        return telefonoEmpresa;
+    }
+
+    public void setTelefonoEmpresa(String telefonoEmpresa) {
+        this.telefonoEmpresa = telefonoEmpresa;
+    }
+
+    public String getNitEmpresa() {
+        return nitEmpresa;
+    }
+
+    public void setNitEmpresa(String nitEmpresa) {
+        this.nitEmpresa = nitEmpresa;
+    }
+}
