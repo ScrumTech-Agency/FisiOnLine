ba8a00e0c7c29022fe1f174520e11299721bdf69
JanetMU <janet_marulanda@yahoo.com>
2022-08-25 23:23:10 -0500

    encapsulamiento de los atributo y correccion tipo variable String

--- src/main/java/com/FisiOnLine/main/models/Empleado.java
+++ src/main/java/com/FisiOnLine/main/models/Empleado.java
@@ -1,12 +1,12 @@
 package com.FisiOnLine.main.models;
 
 public class Empleado {
-    string nombre_empleado;
-    string correo_empleado;
-    string empresa_empleado;
-    string rol_empleado;
+    private String nombre_empleado;
+    private String correo_empleado;
+    private String empresa_empleado;
+    private String rol_empleado;
 
-    public Empleado(string nombre_empleado, string correo_empleado, string empresa_empleado, string rol_empleado) {
+    public Empleado(String nombre_empleado, String correo_empleado, String empresa_empleado, String rol_empleado) {
         this.nombre_empleado = nombre_empleado;
         this.correo_empleado = correo_empleado;
         this.empresa_empleado = empresa_empleado;
@@ -14,35 +14,35 @@ public class Empleado {
 
     }
 
-    public string getNombre_empleado() {
+    public String getNombre_empleado() {
         return nombre_empleado;
     }
 
-    public void setNombre_empleado(string nombre_empleado) {
+    public void setNombre_empleado(String nombre_empleado) {
         this.nombre_empleado = nombre_empleado;
     }
 
-    public string getCorreo_empleado() {
+    public String getCorreo_empleado() {
         return correo_empleado;
     }
 
-    public void setCorreo_empleado(string correo_empleado) {
+    public void setCorreo_empleado(String correo_empleado) {
         this.correo_empleado = correo_empleado;
     }
 
-    public string getEmpresa_empleado() {
+    public String getEmpresa_empleado() {
         return empresa_empleado;
     }
 
-    public void setEmpresa_empleado(string empresa_empleado) {
+    public void setEmpresa_empleado(String empresa_empleado) {
         this.empresa_empleado = empresa_empleado;
     }
 
-    public string getRol_empleado() {
+    public String getRol_empleado() {
         return rol_empleado;
     }
 
-    public void setRol_empleado(string rol_empleado) {
+    public void setRol_empleado(String rol_empleado) {
         this.rol_empleado = rol_empleado;
     }
 }
